a08053d26a833b6a8da50fd0186e259f673de309
codingClaire <suyueyinyin@126.com>
2020-02-22 16:45:06 +0800

    first try

---
+++ time_series/1.py
@@ -0,0 +1,8 @@
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import statsmodels.tsa.api as smt
+import pandas as pd
+import numpy as np 
+import matplotlib.pylab as plt
+import seaborn as sns
+
